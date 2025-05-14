0fed0b707241f8641ae420daaf012609397cd1de
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2025-05-14 14:45:56 +0700

    add send gtm event view_item_list to electronic and furniture

---
+++ javascript/furniture.js
@@ -0,0 +1,26 @@
+import products from "./product-datas.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+  // mengambil data products dan memfilternya
+  const fashionProducts = products.filter(
+    (product) => product.item_category === "furniture"
+  );
+  const itemsForDataLayer = fashionProducts.map((product) => ({
+    item_id: product.item_id,
+    item_name: product.item_name,
+    price: product.price,
+    item_category: product.item_category,
+  }));
+
+  // Kirim event 'view_item' ke dataLayer
+  window.dataLayer = window.dataLayer || [];
+  dataLayer.push({
+    event: "view_item_list",
+    ecommerce: {
+      item_list_id: "furniture_products",
+      item_list_name: "Furniture Products",
+      items: itemsForDataLayer,
+    },
+  });
+});
