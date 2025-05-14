0f381b5f9e8f1274c7d2e0682f8cdcf65b84e3d6
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2025-05-14 16:13:38 +0700

    send gtm event select_promotion

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+console.log("test halo oioi");
+
+const bannerPromotion = document.getElementById("banner-promotion");
+
+bannerPromotion.addEventListener("click", () => {
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+      event: "select_promotion",
+      ecommerce: {
+        creative_name: "Super sale 50% banner",
+        creative_slot: "featured_app_1",
+        promotion_id: "promo-433",
+        promotion_name: "Super sale 50%",
+      },
+    });
+
+    window.location.href = "promotion.html";
+});
